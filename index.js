b09e623d6d4bd095d4117903a43036f97bf8fdc6
Ceren Şahin <cerennssahinn@gmail.com>
2023-06-28 16:19:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    var modifiedCustomerName = customerName.toUpperCase();
+    customerName = modifiedCustomerName;
+}
+var bestCustomer;
+function setBestCustomer() {
+    bestCustomer = "not bob";
+    return bestCustomer;
+}
+console.log(bestCustomer);
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+const leastFavoriteCustomer = "alice";
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "hannah";
+}
